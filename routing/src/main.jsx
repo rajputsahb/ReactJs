787cb671f1e71e269e7baf6e7cf92260331fc789
Himanshu Rajput <rajputhimanshu8958@gmail.com>
2024-07-18 17:46:17 +0530

    First commit

---
+++ routing/src/main.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import App from './App.jsx'
+import About from './components/pages/About.jsx'
+import Contacts from './components/pages/Contacts.jsx'
+import Products from './components/pages/Products.jsx'
+import Home from './components/pages/Home.jsx'
+import ReactDOM from 'react-dom/client'
+import {Route, Outlet, RouterProvider, createBrowserRouter, createRoutesFromElements} from 'react-router-dom'
+
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element={<App/>}>
+
+      <Route index element={<Home/>} />
+      <Route path='about' element={<About/>}   />
+      <Route path='contact' element={<Contacts/>}   />
+     < Route path='products' element={<Products/>}   />
+
+
+    </Route>
+  )
+)
+
+
+
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+  
+   <RouterProvider   router={router}/>
+   
+  </React.StrictMode>,
+)
